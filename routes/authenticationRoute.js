7cf58a03999f4abd9368caf9516c2b0ba67e0572
Daniele Lisi <danielelisi978@gmail.com>
2017-11-17 16:13:31 -0800

    added db utility scripts in the scripts folder

--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -101,7 +101,7 @@ router.post('/login-api', function(req, resp) {
                     req.session.auth = 'HR';
                 }
 
-                if (userEmail === 'mike.plett@osimaritime.com' || userEmail === 'elizabeth.barnard@osimaritime.com') {
+                if (userEmail.includes('@osimaritime.com')) {
                     fetchBamboo(userEmail);
                 } else {
                     req.session.emp_id = result.recordset[0].emp_id;
---
+++ scripts/insertEmpDb.js
@@ -0,0 +1,62 @@
+require('dotenv').config({path: '../.env'});
+
+const sql = require('mssql');
+const jsonfile = require('jsonfile');
+
+// database configurations
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    server: process.env.DB_SERVER_URL,
+    database: 'osi-hr-management'
+};
+
+let empList = jsonfile.readFileSync('./employeeAttributes.json');
+
+console.log(empList.length);
+
+sql.connect(dbConfig, function(err) {
+
+    let dbQuery = 'INSERT INTO employee (emp_id, username, first_name, last_name, emp_number, manager_id, is_approved, emp_type) ' +
+        'VALUES (@emp_id, @username, @first_name, @last_name, @emp_number, @manager_id, @is_approved, @emp_type)';
+
+
+    empList.forEach( function(employee, index) {
+        console.log(employee.emp_id);
+
+        let dbRequest = new sql.Request();
+        dbRequest.input('emp_id', employee.emp_id);
+        dbRequest.input('username', employee.email);
+        dbRequest.input('first_name', employee.firstName);
+        dbRequest.input('last_name', employee.lastName);
+        dbRequest.input('emp_number', employee.emp_number);
+        dbRequest.input('manager_id', employee.manager_id);
+        dbRequest.input('emp_type', 2);
+        dbRequest.input('is_approved', 1);
+
+        dbRequest.query(dbQuery, function(err, result) {
+            if(err) console.log(err);
+            console.log("User inserted: " + index);
+        });
+    });
+
+    // let dbRequest = new sql.Request();
+    // dbRequest.input('emp_id', empList[3].emp_id);
+    // dbRequest.input('username', empList[3].email);
+    // dbRequest.input('first_name', empList[3].firstName);
+    // dbRequest.input('last_name', empList[3].lastName);
+    // dbRequest.input('emp_number', empList[3].emp_number);
+    // dbRequest.input('manager_id', empList[3].manager_id);
+    //
+    //
+    // dbRequest.query(dbQuery, function(err, result) {
+    //     if(err) console.log(err);
+    //
+    //     console.dir(result);
+    // })
+});
+
+
+
+
+
